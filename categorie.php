ba6ac48a95f978b136069a308efdf2242ac2a0e3
Nelox <stell4rz@gmail.com>
2026-04-02 10:01:09 +0200

    catégories presque fini

--- categorie.php
+++ categorie.php
@@ -13,8 +13,19 @@ require_once 'include/db.php';
 <?php 
     include 'include/entete.php';
 
-
-
+if ($_GET['cat'] == 'nouveaux') {	
+	$sth = $dbh->prepare('SELECT idfilm, titre, annee, affiche FROM film ORDER BY idfilm DESC'); // préparer la requête SQL
+	$sth->execute();
+	$films = $sth->fetchAll();
+} else if ($_GET['cat'] == 'notes') {
+	$sth = $dbh->prepare('SELECT idfilm, titre, annee, affiche FROM film 
+						INNER JOIN commentaire ON film.idfilm = commentaire.idfilm
+						GROUP BY film.idfilm
+						ORDER BY AVG(commentaire.note) DESC'); // préparer la requête SQL
+	$sth->execute();
+	$films = $sth->fetchAll();
+} else {
+	$sth = $dbh->prepare('SELECT idfilm, titre, annee, affiche FROM film WHERE genre = ;
 
  * 1. Si c'est les nouveautés, faites une requête SQL qui affiche les N derniers
  *    films ajoutés

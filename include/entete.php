71771c495ae6e015c5b0d76e311e63a8dae4f00a
GamaMKV <gamatrixofficiel@gmail.com>
2026-04-02 10:30:33 +0200

    CSS 003

--- include/entete.php
+++ include/entete.php
@@ -4,6 +4,6 @@
     <li><a href="#">Genres</a></li>
     <li><a href="categorie.php?cat=nouveaux">Nouveautés</a></li>
     <li><a href="categorie.php?cat=notes">Mieux notés</a></li>
-  </ul> 
+  </ul>
   <button class="Ajout2Film" data-label="Ajouter un film !" href="nouveau_film.php">
 </nav>

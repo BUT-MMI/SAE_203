5c229144e659f4ea514e8f85d7835465375207ed
GamaMKV <gamatrixofficiel@gmail.com>
2026-04-03 11:59:44 +0200

    Réorganisation fichier

--- include/entete.php
+++ include/entete.php
@@ -9,34 +9,32 @@ $genres = $sth->fetchAll();
 
 <div class="header-wrapper">
   <header>
-    <!-- GAUCHE : Logo + Nom du site -->
     <a class="logo" href="index.php">
       <img src="img\croutsy.png" alt="Logo" />
       <span>Crousty Movies</span>
     </a>
  
-    <!-- CENTRE : Menu de navigation -->
     <nav>
-      <!-- Menu déroulant Genres -->
       <div class="dropdown">
+
         <button href="categorie.php" class="dropdown-btn">
           Genres <span class="arrow">▼</span>
         </button>
+
         <div class="dropdown-menu">
           <?php foreach ($genres as $genre): ?>
             <a href="categorie.php?cat=<?= urlencode($genre['libelle']) ?>">
               <?= htmlspecialchars($genre['libelle']) ?>
             </a>
           <?php endforeach; ?> <!-- Créer une boucle sans jsp pourquoi -->
-            
         </div>
+        
       </div>
  
       <a href="categorie.php?cat=nouveaux">Nouveautés</a>
       <a href="categorie.php?cat=notes">Mieux notés</a>
  
     </nav>
-    <!-- DROITE : Bouton ajouter un film -->
     <a href="nouveau_film.php" class="AjoutFilm">
     <button class="Ajout2Film" data-label="Ajouter un film !" href="nouveau_film.php" role="button"></button>
     </a>
